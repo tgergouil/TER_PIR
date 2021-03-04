5e1480133a33d95456b97c81198c05b6f1f4be32
Simon Boré <Ssolikel@gmx.fr>
2021-03-04 10:16:49 +0100

    paillier.c cleaner + temporary main + decryption broken

--- main.c
+++ main.c
@@ -1,3 +1,58 @@
-void main(){
+#include "paillier.h"
+#include <gmp.h>
+
+void main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Provide one argument: a random seed\n");
+        return ;
+    } 
+
+    mpz_t intg; //integer
+    gmp_randstate_t rstate; //random state
+    mp_bitcnt_t bcnt = 2048; //bit counter
+    mpz_init2(intg, bcnt); //initiliaze the integer with the right amount of bits
+
+    gmp_randinit_mt(rstate); //initialize random state 
+    char* ptr;
+    unsigned long int seed = strtol(argv[1], &ptr, 10);
+    gmp_randseed_ui(rstate, seed); //set a value to the random state
+
+    size_t size; //size of the integer
+
+    do{
+        mpz_urandomb(intg, rstate, bcnt);
+        size = mpz_sizeinbase (intg, 2); //get a random integer between 0 and 2^bcnt - 1
+    } while(size != 2048);
+
+    gmp_printf ("An mpz with %lu bits: %Zd\n", size, intg);
+    
+    mpz_t p; //integer
+    mpz_init2(p, bcnt);
+    
+    mpz_nextprime(p, intg); //takes the next prime
+
+    mpz_t q;
+    mpz_init2(q, bcnt);
+    mpz_nextprime(q, p);
+
+    mpz_t n;
+    mpz_init(n);
+    mpz_mul(n, p, q);
+
+    /** DELETE BEFORE **/
+
+    /** TEST ENCRYPTION **/
+    printf("\n\n*****************************************************************\n\n");
+    mpz_t cyphered, plain;
+
+    mpz_init(cyphered);
+    mpz_init2(plain, 1234);
+    
+    encrypt(cyphered, n, plain);
+    
+    /** TEST DECRYPTION **/
+    decrypt(plain, n, p, q, cyphered);
+    gmp_printf("%Zd\n", plain);
+
     return ;
 }
--- paillier.c
+++ paillier.c
@@ -2,61 +2,87 @@
 #include <gmp.h>
 
 void encrypt(mpz_t cyphered, mpz_t n, mpz_t plain){
-    mpz_t r; //random integer
+
+    /********************** INITIALIZATIONS *****************/
+    
+    mpz_t r, n_square, g, c1, c2; //random integer
+    mpz_inits(r, n_square, g, c1, c2, NULL);
+    
+    /********************** ENCRYPTION **********************/
+
     gmp_randstate_t rstate; //random state
 
     gmp_randinit_mt(rstate); //initialize random state 
-    gmp_randseed_ui(rstate, 1000); //set the seed, seed is const for debugging purposes 
+    gmp_randseed_ui(rstate, 565565865); //set the seed, seed is const for debugging purposes 
     /** TODO CHANGE CONSTANT SEED TO RANDOM ONE WITH /dev/random **/ 
-
+    
     mpz_urandomm(r, rstate, n - 1);
     mpz_add_ui(r, r, 1); //random in [0, n-2] now is in [1, n-1]
 
-    mpz_t n_square;
-    mpz_pow_ui(n_square, n, 2); // n_square = n^2
+    // n_square = n^2
+    mpz_pow_ui(n_square, n, 2);
 
-    mpz_t g;
-    mpz_add_ui(g, n, 1); //g = n + 1
+    //g = n + 1 
+    mpz_add_ui(g, n, 1); 
 
-    mpz_t c1;
-    mpz_powm(c1, g, plain, n_square); // c1 = g^m mod n^2
+    // c1 = g^m mod n^2
+    mpz_powm(c1, g, plain, n_square); 
 
-    mpz_t c2;
-    mpz_powm(c2, r, n, n_square); // c2 = r^n mod n^2
+    // c2 = r^n mod n^2
+    mpz_powm(c2, r, n, n_square); 
 
+    //cyphered = c1 * c2
     mpz_mul(cyphered, c1, c2); 
     mpz_mod(cyphered, cyphered, n_square); // cyphered = g^m * r^n mod n^2
+
+    /********************** CLEARING ************************/
+
+    mpz_clears(r, n_square, g, c1, c2, NULL);
 }
 
 void decrypt(mpz_t plain, mpz_t n, mpz_t p, mpz_t q, mpz_t c){
-    mpz_t n_square;
-    mpz_pow_ui(n_square, n, 2); // n_square = n^2
 
-    mpz_t g;
-    mpz_add_ui(g, n, 1); //g = n + 1
+    /********************** INITIALIZATIONS *****************/
 
-    mpz_t p_minus;
+    mpz_t n_square, g, p_minus, q_minus, c1, c2, lambda;
+    mpz_inits(n_square, g, p_minus, q_minus, c1, c2, lambda, NULL);
+
+    /********************** DECRYPTION **********************/
+    gmp_printf("TEST c : %Zd\n\n", c);
+    // n_square = n^2
+    mpz_pow_ui(n_square, n, 2); 
+
+    //g = n + 1
+    mpz_add_ui(g, n, 1); 
+
+    // p_minus = p - 1
     mpz_sub_ui(p_minus, p, 1);
 
-    mpz_t q_minus;
+    //q_minus = q - 1
     mpz_sub_ui(q_minus, q, 1);
+    
+    //lambda = lcm(p-1, q-1)
+    mpz_lcm(lambda, p_minus, q_minus); 
 
-    mpz_t lambda;
-    mpz_lcm(lambda, p_minus, q_minus); //lambda = lcm(p-1, q-1)
+    //c1 = c^lambda mod n^2
+    mpz_powm(c1, c, lambda, n_square); 
+    gmp_printf("TEST c1 : %Zd\n\n", c1);
+    gmp_printf("TEST l  : %Zd\n", lambda);
 
-    mpz_t c1;
-    mpz_powm(c1, c, lambda, n_square); //c1 = c^lambda mod n^2
-
-    mpz_t c2;
-    mpz_powm(c2, g, lambda, n_square); //c2 = g^lambda mod n^2
+    //c2 = g^lambda mod n^2
+    mpz_powm(c2, g, lambda, n_square); 
 
     mpz_sub_ui(c1, c1, 1);
-    mpz_div(c1, c1, n); //L(c1, n) 
+    mpz_div(c1, c1, n); //L(c1, n)
     mpz_sub_ui(c2, c2, 1);
     mpz_div(c2, c2, n); //L(c2, n)
-    
+
     mpz_div(plain, c1, c2);
     mpz_mod(plain, plain, n);
+
+    /********************** CLEARING ************************/
+
+    mpz_clears(n_square, g, p_minus, q_minus, c1, c2, lambda, NULL);
 }
 
 
