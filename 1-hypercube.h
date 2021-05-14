ce38921dbb2c372a156def142ad48295028e00ba
BOUQUEROT <45669004+BOUQUEROT@users.noreply.github.com>
2021-05-14 23:20:49 +0200

    1-hyp
    1-hyp

--- 1-hypercube.h
+++ 1-hypercube.h
@@ -3,4 +3,5 @@
 #include <gmp.h>
 
 void Hyp1(mpz_t* m,mpz_t n,int N,mpz_t* c, mpz_t g, mpz_t n_square);
-void sHyp1(mpz_t* ct,int N ,mpz_t* x,mpz_t plain, mpz_t n, mpz_t p, mpz_t q, mpz_t c, mpz_t g, mpz_t n_square, mpz_t p_minus, mpz_t q_minus, mpz_t lambda);
+void phif(mpz_t* ct,int N ,mpz_t* x, mpz_t c, mpz_t n_square  );
+void sHyp1(int N ,mpz_t plain, mpz_t n, mpz_t p, mpz_t q, mpz_t c, mpz_t g, mpz_t n_square,  mpz_t lambda);
